db48b0ab3817fbaf5aced28ee9d421f88d5a5b41
itsjoesullivan <itsjoesullivan@gmail.com>
2014-08-31 19:22:26 -0400

    further

--- sv.go
+++ sv.go
@@ -4,14 +4,26 @@ import (
   "fmt"
   "io/ioutil"
   "net/http"
+  "log"
+  "time"
 )
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+  var startTime = time.Now()
   filename := r.URL.Path[1:]
+  if r.URL.Path == "/" {
+    filename = "index.html"
+  }
+  var status = "200";
   body, err := ioutil.ReadFile(filename)
   if err != nil {
+    status = "404";
+    fmt.Fprintf(w, "error.")
+  } else {
+    fmt.Fprintf(w, string(body[:]))
   }
-  fmt.Fprintf(w, string(body[:]))
+  var duration = string(time.Now().Sub(startTime).Nanoseconds() / 1000)
+  log.Printf(r.Method + " " + filename + " " + status + " " + duration + "ms")
 }
 
 func main() {
